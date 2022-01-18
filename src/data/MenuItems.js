d47b64a59418602f328754af2c9871c2932e3d14
ManuelEdesa <war4zone@gmail.com>
2022-01-18 10:47:05 +0100

    2 files modified

--- src/data/MenuItems.js
+++ src/data/MenuItems.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import { Home } from '../components/Home';
+import { PaginaListas } from '../components/PaginaListas';
+import { Perfil } from '../components/Perfil';
 
-export function Perfil() {
-  return (
-    <div className="main-site">
-      <h1>Página de perfil</h1>
-    </div>
-  );
-}
+export const MenuItems = [
+  {
+    id: 1,
+    path: '/',
+    title: 'Home',
+    component: Home,
+  },
+  {
+    id: 2,
+    path: '/listas',
+    title: 'Listas de tareas',
+    component: PaginaListas,
+  },
+  {
+    id: 3,
+    path: '/perfil',
+    title: 'Perfil',
+    component: Perfil,
+  },
+];
+
+/*
+  path
+  id
+  title
+  component
+
+*/
