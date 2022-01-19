cb31d5cfec4ed00614f860d569b1c588ec9aa86e
ManuelEdesa <war4zone@gmail.com>
2022-01-19 12:53:54 +0100

    1 file modified

--- src/data/DatosSeries.js
+++ src/data/DatosSeries.js
@@ -21,15 +21,18 @@ export const DatosTablaSeries = [
     fechaestreno: '23 de noviembre de 2015',
     temporadas: '6',
     capitulos: '62',
-  },
-  {
-    titulo: '0000 LOL',
-    fechaestreno: 'Peugeot',
-    temporadas: 'Partner',
-    capitulos: 'rojo',
     imagen:
       'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/the-witcher-1621253612.jpeg',
-    descripción: 'Una serie impresionante',
+    descripción: 'Una aventura en el espacio',
+  },
+  {
+    titulo: 'The Office',
+    fechaestreno: '	24 de marzo de 2005',
+    temporadas: '9',
+    capitulos: '201',
+    imagen:
+      'https://i.blogs.es/62425d/the-office/1024_2000.jpg',
+    descripción: 'La mejor comedia que existe',
   },
   {
     titulo: '0000 LOL',
